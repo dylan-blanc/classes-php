7fb571ab100e5c9ed72695a923a8a9045046ab61
Dylan <dylan.blanc@laplateforme.io>
2025-11-13 13:50:11 +0100

    maj db et base html

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$dbname = 'nom_de_la_base';
+$dbname = 'classes';
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
@@ -10,7 +10,23 @@ if ($conn->connect_error) {
     die('Erreur de connexion (' . $conn->connect_errno . ') ' . $conn->connect_error);
 }
 
-
-
+require_once 'src/user.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blabla</title>
+</head>
+
+<body>
+    <h1>TEST</h1>
+
+
+</body>
+
+</html>
+    
--- src/db/create_table_utilisateurs.php
+++ src/db/create_table_utilisateurs.php
@@ -1,10 +1,14 @@
 <?php
 
-"CREATE TABLE utilisateurs (
+CREATE DATABASE IF NOT EXISTS classes;
+USE classes;
+
+CREATE TABLE utilisateurs (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     login VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL
-)";
+);
+
